044ef8766772bcc6aa6d0267db5999338ac60850
LibrazionPc <nwajeizion393@gmail.com>
2025-09-17 09:50:41 +0100

    Fix base issues

--- app/models/basemodel.py
+++ app/models/basemodel.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Column, DateTime, UUID, Boolean
 from uuid import uuid4
+from app.db.postgres_db_conn import Base
 
-class BaseModel:
+class BaseModel(Base):
+    __abstract__ = True
+
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid4)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)

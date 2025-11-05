d535321c64895c7a6822453885ed2bf74d203968
pcnerd <ajayihabeeb977@gmail.com>
2025-11-05 12:13:13 +0100

    🐛 fix: return the correct error when user uses wrong product categories

--- app/service/shipment_info_crud.py
+++ app/service/shipment_info_crud.py
@@ -16,15 +16,6 @@ class ShipmentInfoCRUDService:
     ) -> JSONResponse:
 
         try:
-            shipment: ShipmentInfo | None = store.shipment_info
-
-            if shipment:
-                return response_builder(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    status="error",
-                    message="Shipment info already exists for this store.",
-                )
-
             data["store_id"] = store.id
             new_shipment = await ShipmentInfo.create(data, db)
             new_shipment = new_shipment.to_dict()
@@ -47,17 +38,19 @@ class ShipmentInfoCRUDService:
 
     async def get(self, store: Store) -> JSONResponse:
         try:
-            shipment: ShipmentInfo | None = store.shipment_info
-            if not shipment:
+            shipments: ShipmentInfo | None = store.shipment_info
+            if not shipments:
                 return response_builder(
                     status_code=status.HTTP_404_NOT_FOUND,
                     status="error",
                     message="Shipment info not found.",
                 )
+            data = []
+            for shipment in shipments:
+                shipment = shipment.to_dict()
+                shipment["user_id"] = str(store.user_id)
+                data.append(ShipmentsInfoResponse(**shipment))
 
-            shipment = shipment.to_dict()
-            shipment["user_id"] = str(store.user_id)
-            data = ShipmentsInfoResponse(**shipment)
             return response_builder(
                 status_code=status.HTTP_200_OK,
                 status="success",
@@ -73,19 +66,30 @@ class ShipmentInfoCRUDService:
             )
 
     async def update(
-        self, db: AsyncSession, data: dict[str, Any], store: Store
+        self, shipment_id: str, db: AsyncSession, data: dict[str, Any], store: Store
     ) -> JSONResponse:
 
         try:
-            shipment: ShipmentInfo | None = store.shipment_info
-            if not shipment:
+            shipments: ShipmentInfo | None = store.shipment_info
+            if not shipments:
                 return response_builder(
                     status_code=status.HTTP_404_NOT_FOUND,
                     status="error",
                     message="Shipment info not found.",
                 )
+            shipment_to_update = None
+            for shipment in shipments:
+                if str(shipment.id) == shipment_id:
+                    shipment_to_update = shipment
+                    break
+            else:
+                return response_builder(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    status="error",
+                    message="shipment not found",
+                )
 
-            updated_shipment = await shipment.update(db, data)
+            updated_shipment = await shipment_to_update.update(db, data)
             updated_shipment = updated_shipment.to_dict()
             updated_shipment["user_id"] = str(store.user_id)
             data = ShipmentsInfoResponse(**updated_shipment)
@@ -103,17 +107,29 @@ class ShipmentInfoCRUDService:
                 message="An error occurred while updating shipment info.",
             )
 
-    async def delete(self, db: AsyncSession, store: Store) -> JSONResponse:
+    async def delete(
+        self, shipment_id: str, db: AsyncSession, store: Store
+    ) -> JSONResponse:
         try:
-            shipment: ShipmentInfo | None = store.shipment_info
-            if not shipment:
+            shipments: ShipmentInfo | None = store.shipment_info
+            if not shipments:
                 return response_builder(
                     status_code=status.HTTP_404_NOT_FOUND,
                     status="error",
                     message="Shipment info not found.",
                 )
 
-            await ShipmentInfo.delete_permanently_by_id(id=shipment.id, db=db)
+            for shipment in shipments:
+                if str(shipment.id) == shipment_id:
+                    break
+            else:
+                return response_builder(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    status="error",
+                    message="shipment not found",
+                )
+
+            await ShipmentInfo.delete_permanently_by_id(id=shipment_id, db=db)
             return response_builder(
                 status_code=status.HTTP_200_OK,
                 status="success",

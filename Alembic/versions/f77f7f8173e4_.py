05e4bbda791183617542850987ba3c0bb983c2fa
znwajei <zion.nwajei@redtechlimited.com>
2026-05-02 15:38:34 +0100

    Git issues

---
+++ Alembic/versions/f77f7f8173e4_.py
@@ -0,0 +1,28 @@
+"""empty message
+
+Revision ID: f77f7f8173e4
+Revises: 6ac704c2b309, d2bd3ecd4370
+Create Date: 2026-05-02 15:26:13.468896
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f77f7f8173e4'
+down_revision: Union[str, Sequence[str], None] = ('6ac704c2b309', 'd2bd3ecd4370')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

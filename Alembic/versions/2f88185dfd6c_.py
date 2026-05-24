76c83d2527c7dbbe822db770540989d618275b79
znwajei <zion.nwajei@redtechlimited.com>
2026-05-24 21:02:00 +0100

    Alembic heads

---
+++ Alembic/versions/2f88185dfd6c_.py
@@ -0,0 +1,28 @@
+"""empty message
+
+Revision ID: 2f88185dfd6c
+Revises: 284346085d2a, 84de23a34a03
+Create Date: 2026-05-24 21:01:36.565422
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2f88185dfd6c'
+down_revision: Union[str, Sequence[str], None] = ('284346085d2a', '84de23a34a03')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

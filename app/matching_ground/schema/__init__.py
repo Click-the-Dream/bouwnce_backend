4ee98fd42e47ac6567b78551bd3decbc370f1f48
znwajei <zion.nwajei@redtechlimited.com>
2026-05-25 17:11:28 +0100

    CI Issues

--- app/matching_ground/schema/__init__.py
+++ app/matching_ground/schema/__init__.py
@@ -1,2 +1,11 @@
-from app.matching_ground.schema.buddy_search import BuddyMatch, BuddySearchResult
-from app.matching_ground.schema.matching import MatchInputs
+from app.matching_ground.schema.buddy_search import (
+    BuddyMatch as BuddyMatch,
+    BuddySearchResult as BuddySearchResult,
+)
+from app.matching_ground.schema.matching import MatchInputs as MatchInputs
+
+__all__ = [
+    "BuddyMatch",
+    "BuddySearchResult",
+    "MatchInputs",
+]

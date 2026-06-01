075ab4895868dc9cf7cb731da9ee6fd357a81716
Pcnerd <ajayihabeeb977@gmail.com>
2026-06-01 02:06:46 +0100

    🐛 fix: merge alembic mulitple heads

---
+++ Alembic/versions/b39d8a61427d_.py
@@ -0,0 +1,28 @@
+"""empty message
+
+Revision ID: b39d8a61427d
+Revises: c3f0b6f6f9a1, dfcf2513ffe4
+Create Date: 2026-06-01 02:06:04.176399
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b39d8a61427d'
+down_revision: Union[str, Sequence[str], None] = ('c3f0b6f6f9a1', 'dfcf2513ffe4')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

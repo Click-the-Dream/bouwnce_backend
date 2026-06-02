83ab546854efd3170a0fe0d70649b840652b10f3
znwajei <zion.nwajei@redtechlimited.com>
2026-06-02 14:01:21 +0100

    ci db alembic

--- Alembic/versions/515f208cdc76_allembic.py
+++ Alembic/versions/515f208cdc76_allembic.py
@@ -6,16 +6,13 @@ Create Date: 2026-04-22 02:12:57.867275
 
 """
 
-from typing import Sequence, Union
-
-import sqlalchemy as sa
-from alembic import op
+from collections.abc import Sequence
 
 # revision identifiers, used by Alembic.
 revision: str = "515f208cdc76"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = "45affa483b69"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
